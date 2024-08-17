c0460b61954fc5b41079cb3824ed865242c3923a
Rasel Miah <rasel6041@gmail.com>
2024-08-17 01:49:48 +0100

    first commit

---
+++ data/index.ts
@@ -0,0 +1,217 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  /* { name: "Projects", link: "#projects" }, */
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    /* title: "Currently Studying in LIU ", */
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh] text-black",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.png",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Currently Studying in LIU",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "General Knowledge",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  /* {
+    id: 4,
+    title: "Dedicated team member with a passion for exceptional service.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  }, */
+/* 
+  {
+    id: 5,
+    title: "Currently Working in Sweden",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  }, */
+  /* {
+    id: 6,
+    title: "Do you want to hire me or work with me.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  }, */
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Linköping University",
+    des: "Swedish language courses for Exchange students",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Linköping University",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Linköping University",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Linköping University",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/liu.se",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Robin was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Robin's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Robin is the ideal partner.",
+    name: "Rahul Miah",
+    title: "Director of Team Black-Hole",
+  },
+  {
+    quote:
+      "Collaborating with Robin was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Robin's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Robin is the ideal partner.",
+    name: "Polan Miah",
+    title: "Director of Team Black-Hole",
+  },
+  {
+    quote:
+      "Collaborating with Robin was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Robin's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Robin is the ideal partner.",
+   name: "Sobur Miah",
+    title: "Director of Team Black-Hole",
+  },
+  {
+    quote:
+      "Collaborating with Robin was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Robin's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Robin is the ideal partner.",
+    name: "Robin Miah",
+    title: "Director of Team Black-Hole",
+  },
+  {
+    quote:
+     "Collaborating with Robin was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Robin's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Robin is the ideal partner.",
+     name: "Rasel",
+    title: "Director of Team Black-Hole",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev - JSM Tech",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  /* {
+    id: 1,
+    img: "/git.svg",
+  }, */
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
